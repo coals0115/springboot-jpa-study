411b984c0d8426942927867d431a4b39757a6616
coals0115 <slaoc0115@gmail.com>
2024-02-24 15:34:17 +0900

    2-16. thymeleaf 사용하기

--- ch2/src/main/java/com/fastcampus/ch2/HomeController.java
+++ ch2/src/main/java/com/fastcampus/ch2/HomeController.java
@@ -1,9 +1,11 @@
 package com.fastcampus.ch2;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 // 1. 원격 프로그램으로 등록
 @Controller
@@ -14,9 +16,13 @@ public class HomeController { // 원격 프로그램
     }
 
     // 2. URL과 메서드를 연결
-    @RequestMapping("/hello")
-    public String main(String[] args) {
-        return "hello";
-    }
+    @GetMapping("/test")
+    public String test(Model model) {
+        model.addAttribute("lastName", "Bang");
+        model.addAttribute("firstName", "Chaemin");
+        model.addAttribute("list", Arrays.asList("aaa", "bbb", "ccc", "ddd", "eee"));
+        model.addAttribute("bno", 123);
 
+        return "test";
+    }
 }
