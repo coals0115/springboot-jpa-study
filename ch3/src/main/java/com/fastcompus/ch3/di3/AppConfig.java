fd56f8a551a3902d64008a863c43e0e780774120
coals0115 <slaoc0115@gmail.com>
2024-02-24 16:17:54 +0900

    3-4. Bean과 ApplicationContext

---
+++ ch3/src/main/java/com/fastcompus/ch3/di3/AppConfig.java
@@ -0,0 +1,18 @@
+package com.fastcompus.ch3.di3;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+// 객체를 생성해서 사용하기 위함이 아닌 설정을 위한 클래스
+@Configuration // 일반 클래스가 아닌 설정 클래스라는 뜻
+public class AppConfig {
+    @Bean
+    // 메서드 이름이 키로 저장..
+    Car car() { return new Car(); } // 기본은 싱글톤이기 때문에 매번 getBean 한다고 새로운 객체가 만들어지는 건 아님
+    @Bean
+    @Scope("prototype")
+    Engine engine() { return new Engine(); }
+    @Bean
+    Door door() { return new Door(); }
+}
---
+++ ch3/src/main/java/com/fastcompus/ch3/di3/Main.java
@@ -0,0 +1,47 @@
+package com.fastcompus.ch3.di3;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+class Car {
+    Engine engine;
+    Door door;
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "engine=" + engine +
+                ", door=" + door +
+                '}';
+    }
+}
+
+class Engine {}
+
+class Door {}
+
+public class Main {
+    public static void main(String[] args) {
+        // AC를 생성하면서 설정 파일에 있는 객체를 빈으로 등록
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+//        Car car = (Car) ac.getBean("car"); // byName 객체(빈)을 조회
+        Car car = ac.getBean("car", com.fastcompus.ch3.di3.Car.class); // 위와 동일
+        Engine engine = ac. getBean (Engine.class) ;
+        Engine engine2 = ac. getBean (Engine.class);
+        Engine engine3 = ac. getBean (Engine.class);
+
+        System.out.println("car = " + car);
+        System.out.println("engine = " + engine);
+        System.out.println("engine2 = " + engine2);
+        System.out.println("engine3 = " + engine3);
+
+        System.out.println("ac.getBeanDefinitionCount() = " + ac.getBeanDefinitionCount()); // Bean이 몇 개 등록되어있는지
+        System.out.println("Arrays.toString(ac.getBeanDefinitionNames()) = " + Arrays.toString(ac.getBeanDefinitionNames()));
+        System.out.println("ac.containsBeanDefinition(\"engine\") = " + ac.containsBeanDefinition("engine"));
+        System.out.println("ac.isSingleton(\"engine\") = " + ac.isSingleton("engine"));
+    }
+}
