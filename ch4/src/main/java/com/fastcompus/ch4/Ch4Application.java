92139095e638e916ee366f4c29bbb286b93f7cd7
coals0115 <slaoc0115@gmail.com>
2024-02-26 10:56:57 +0900

    4-1. JPA의 개요와 설정

--- ch4/src/main/java/com/fastcompus/ch4/Ch4Application.java
+++ ch4/src/main/java/com/fastcompus/ch4/Ch4Application.java
@@ -1,13 +1,28 @@
 package com.fastcompus.ch4;
 
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ch4Application {
+public class Ch4Application implements CommandLineRunner {
+    @Autowired
+    EntityManagerFactory emf;
 
     public static void main(String[] args) {
-        SpringApplication.run(Ch4Application.class, args);
+//        SpringApplication.run(Ch4Application.class, args);
+
+        // 이렇게 해두면 톰캣이 안 뜬다.
+        SpringApplication app = new SpringApplication(Ch4Application.class);
+        app.setWebApplicationType(WebApplicationType.NONE);
+        app.run(args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("emf = " + emf);
+    }
 }
