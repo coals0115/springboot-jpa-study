4c42b176398bbfcf6e52dba75033d2fe897b0205
coals0115 <slaoc0115@gmail.com>
2024-04-13 17:36:47 +0900

    섹션 5. 연관관계 매핑 기초 - 양방향 연관관계과 연관관계의 주인 1-기본

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,8 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
     public static void main(String[] args) {
         // 팩토리를 만드는 순간 DB 연결된 것..
@@ -12,30 +14,27 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            team.setName("TeamA");
-            em.persist(team); // persist를 하면 id에 값이 들어간다.
-            // persist를 하면 무조건 PK가 세팅이 되고 영속 상태가 된다.
-
             Member member = new Member();
             member.setName("member1");
             member.setTeam(team); // JPA가 team에서 PK를 꺼내 FK에 insert 할 때 값을 넣어준다.
             em.persist(member);
 
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team); // persist를 하면 id에 값이 들어간다.
+            // persist를 하면 무조건 PK가 세팅이 되고 영속 상태가 된다.
+
             em.flush(); // flush로 영속성 컨텍스트에 있는 걸 DB에 쿼리를 날린다. / 싱크를 맞춤
             em.clear(); // 영속성 컨텍스트 초기화
 
             Member findMember = em.find(Member.class, member.getId()); // 기본값일 시에(EAGER?) TEAM을 같이 가져온다.(조인해서)
-            Team findTeam = findMember.getTeam();
-            System.out.println("findTeam = " + findTeam);
 
-            // 새로운 팀B
-            Team teamB = new Team();
-            teamB.setName("TeamB");
-            em.persist(teamB);
+            List<Member> members = findMember.getTeam().getMembers();
+            for (Member member1 : members) {
+                System.out.println("member1 = " + member1);
+            }
 
-            // 회원1에 새로운 팀B 설정
-            member.setTeam(teamB);
+
 
             tx.commit();
 
--- ex1-hello-jpa/src/main/java/hellojpa/Team.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Team.java
@@ -3,12 +3,18 @@ package hellojpa;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
     @Id @GeneratedValue
     private Long id;
     private String name;
+    @OneToMany(mappedBy = "team") // 나는 반대편에서 team으로 매핑이 되어있다..
+    private List<Member> members = new ArrayList<>(); // add 시 NPE가 안 뜨기 때문에 관례료 많이 쓴다.
 
     public Long getId() {
         return id;
@@ -26,11 +32,11 @@ public class Team {
         this.name = name;
     }
 
-    @Override
-    public String toString() {
-        return "Team{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
     }
 }
