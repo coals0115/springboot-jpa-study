da47d5c631b3efce49b62d4296a6804d8710c84d
chaemin <slaoc0115@gmail.com>
2024-04-30 09:01:17 +0900

    섹션4. 회원 도메인 개발

---
+++ jpashop2/src/main/java/jpabook2/jpashop2/service/MemberService.java
@@ -0,0 +1,37 @@
+package jpabook2.jpashop2.service;
+
+import jakarta.transaction.Transactional;
+import java.util.List;
+import jpabook2.jpashop2.domain.Member;
+import jpabook2.jpashop2.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    // 회원 전체 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+}
