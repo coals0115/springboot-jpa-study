14dd25735dd87ebf17d971ff026533482cd67ee6
coals0115 <slaoc0115@gmail.com>
2024-05-01 09:50:37 +0900

    섹션5. 상품 도메인 개발

--- jpashop2/src/main/java/jpabook2/jpashop2/domain/Item.java
+++ jpashop2/src/main/java/jpabook2/jpashop2/domain/Item.java
@@ -1,6 +1,7 @@
 package jpabook2.jpashop2.domain;
 
 import jakarta.persistence.*;
+import jpabook2.jpashop2.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,8 +11,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
-@Getter
-@Setter
+@Getter @Setter
 public abstract class Item {
     @Id @GeneratedValue
     @Column(name = "item_id")
@@ -26,4 +26,19 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    /**
+     * stock 증가
+     */
+    // stockQuantity를 변경할 일이 있으면, 밖에서 가져와서 계산해서 setter로 변경하는 게 아니라
+    // 아래와 같은 비즈니스 메서드를 통해 변경을 해야 함(내부에서) => 객체지향적
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+    }
 }
