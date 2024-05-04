6daeb145dacc3914436b48f180da8eddb2ba3c45
coals0115 <slaoc0115@gmail.com>
2024-05-04 11:41:44 +0900

    섹션7. 웹 계층 개발 - 상품 목록

--- jpashop2/src/main/java/jpabook2/jpashop2/controller/ItemController.java
+++ jpashop2/src/main/java/jpabook2/jpashop2/controller/ItemController.java
@@ -1,6 +1,7 @@
 package jpabook2.jpashop2.controller;
 
 import jpabook2.jpashop2.domain.Book;
+import jpabook2.jpashop2.domain.Item;
 import jpabook2.jpashop2.service.ItemService;
 import jpabook2.jpashop2.web.BookForm;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -30,6 +33,13 @@ public class ItemController {
         book.setIsbn(form.getIsbn());
 
         itemService.saveItem(book);
-        return "redirect:/items";
+        return "redirect:/";
+    }
+
+    @GetMapping("/items")
+    public String list(Model model){
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
     }
 }
