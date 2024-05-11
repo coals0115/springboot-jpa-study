9bb9b54cc7f6a4e789da522e4f7e4a7a10d8d691
coals0115 <slaoc0115@gmail.com>
2024-05-11 15:23:49 +0900

    섹션4. API 개발 고급 - 컬렉션 조회 최적화) 주문 조회 V2: 엔티티를 DTO로 변환

--- jpashop2/src/main/java/jpabook2/jpashop2/api/OrderApiController.java
+++ jpashop2/src/main/java/jpabook2/jpashop2/api/OrderApiController.java
@@ -1,21 +1,30 @@
 package jpabook2.jpashop2.api;
 
+import jpabook2.jpashop2.domain.Address;
 import jpabook2.jpashop2.domain.Order;
 import jpabook2.jpashop2.domain.OrderItem;
+import jpabook2.jpashop2.domain.OrderStatus;
 import jpabook2.jpashop2.repository.OrderRepository;
 import jpabook2.jpashop2.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class OrderApiController {
     private final OrderRepository orderRepository;
 
-
+    /**
+     * V1. 엔티티 직접 노출
+     * - Hibernate5Module 모듈 등록, LAZY=null 처리
+     * - 양방향 관계 문제 발생 -> @JsonIgnore
+     */
     @GetMapping("/api/v1/orders")
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
@@ -33,5 +42,51 @@ public class OrderApiController {
         }
 
         return all;
+    }
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> collect = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return collect;
+    }
+
+    @Data
+    static class OrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .toList();
+            // 엔티티를 래핑해서 보여주는 것도 절대 XX OrderItem도 DTO로 바꿔야 한다.
+//            order.getOrderItems().stream().forEach(o -> o.getItem().getName());
+
+        }
+    }
+
+    @Data
+    static class OrderItemDto {
+        private String itemName; //상품 명
+        private int orderPrice; //주문 가격
+        private int count; //주문 수량
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
     }
 }
