39f6cdb3ab55aa60bee6672582439f5d059ef0df
coals0115 <slaoc0115@gmail.com>
2024-05-11 15:54:46 +0900

    섹션4. API 개발 고급 - 컬렉션 조회 최적화) 주문 조회 V3: 엔티티를 DTO로 변환 - 페치 조인 최적화

--- jpashop2/src/main/java/jpabook2/jpashop2/api/OrderApiController.java
+++ jpashop2/src/main/java/jpabook2/jpashop2/api/OrderApiController.java
@@ -15,6 +15,27 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * V1. 엔티티 직접 노출
+ * - 엔티티가 변하면 API 스펙이 변한다.
+ * - 트랜잭션 안에서 지연 로딩 필요
+ * - 양방향 연관관계 문제
+ *
+ * V2. 엔티티를 조회해서 DTO로 변환 (fetch join 사용X)
+ * - 트랜잭션 안에서 지연 로딩 필요
+ *
+ * V3. 엔티티를 조회해서 DTO로 변환 (fetch join 사용O)
+ * - 페이징 시에는 N 부분을 포기해야 함 (대신에 batch fetch size 옵션 주면 N -> 1 쿼리로 변경 가능)
+ *
+ * V4. JPA에서 DTO로 바로 조회, 컬렉션 N 조회 (1 + N Query)
+ * - 페이징 가능
+ *
+ * V5. JPA에서 DTO로 바로 조회, 컬렉션 1 조회 최적화 버전 (1 + 1 Query)
+ * - 페이징 가능
+ *
+ * V6. JPA에서 DTO로 바로 조회, 플랫 데이터 (1 Query)
+ * - 페이징 불가능
+ */
 @RestController
 @RequiredArgsConstructor
 public class OrderApiController {
@@ -51,6 +72,19 @@ public class OrderApiController {
                 .map(o -> new OrderDto(o))
                 .collect(Collectors.toList());
         return collect;
+    }
+
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> ordersV3() {
+        List<Order> allWithItem = orderRepository.findAllWithItem();
+        for (Order order : allWithItem) {
+            System.out.println("order ref=" + order + " order.getId() = " + order.getId());
+        }
+        List<OrderDto> collect = allWithItem.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return collect;
+
     }
 
     @Data
