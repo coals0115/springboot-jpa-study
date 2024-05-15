a2f3dd0c3680e7ed9597bf36a51621480cf193da
coals0115 <slaoc0115@gmail.com>
2024-05-15 16:42:00 +0900

    섹션1. 프로젝트 환경설정

--- querydsl/src/main/java/study/querydsl/QuerydslApplication.java
+++ querydsl/src/main/java/study/querydsl/QuerydslApplication.java
@@ -1,7 +1,10 @@
 package study.querydsl;
 
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
 public class QuerydslApplication {
--- querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,44 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import static org.assertj.core.api.Assertions.*;
+
 
 @SpringBootTest
+@Transactional
+@Commit
 class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+//		QHello qHello = new QHello("h");
+		QHello qHello = QHello.hello;
+
+		// QueryDsl을 쓸 때 Query와 관련된 건 다 Qtype을 써야 한다.
+		Hello result = queryFactory
+				.selectFrom(qHello)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
+
 	}
 
 }
